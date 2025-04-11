f68eea7624160b7f7d56fe6be6c1988ba61674a0
Sheila Samita <samitasheila1@gmail.com>
2025-04-11 13:38:44 +0300

    named import and export syntax

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -1,7 +1,10 @@
-import { username, city, image } from "../data/user";
+import { username, city, image } from "../components/user";  // ✅ Correct path
+
+
 
 test("username, city, and image are all exported", () => {
   expect(username).toEqual("Liza");
   expect(city).toEqual("New York");
   expect(image).toEqual("https://i.imgur.com/mV8PQxj.gif");
 });
+
